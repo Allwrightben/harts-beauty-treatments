2092a3a0eda1c59f914ed208df05bdb7bbcabd14
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-07-01 20:09:40 +0100

    Add secret key

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mph8d(6osxbx(&nnzh-a679uoe#q7nh)jd)v_77^$a#^6j7l=n'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
