3667227bc9456a95ded19ab1fbfbbf833ecf5ac1
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-07-01 20:53:10 +0100

    Add treatments model

--- treatments/models.py
+++ treatments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Treatment(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    status = models.IntegerField(choices=STATUS, default=0)
+    
