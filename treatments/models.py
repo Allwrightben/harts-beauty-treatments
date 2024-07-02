6de7e375a52d23156dc2b6bf680dd1729fdd6c92
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-07-02 10:56:12 +0100

    Update model with one additioal field

--- treatments/models.py
+++ treatments/models.py
@@ -8,6 +8,7 @@ class Treatment(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
+    excerpt = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     status = models.IntegerField(choices=STATUS, default=0)
     
